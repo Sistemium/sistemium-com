194bd1c2a4fc4e025b169839571ba08488f41cab
Alexander Levin <alevin.ru@gmail.com>
2018-02-28 10:51:44 +0200

    resize.directive

--- src/app/components/resize.directive.js
+++ src/app/components/resize.directive.js
@@ -2,35 +2,75 @@
 
 (function () {
 
-  function resize($window, $uibPosition, $timeout) {
+  function resize($window, $uibPosition, $timeout, $rootScope) {
+
+    const SCREEN_XS_MAX = 768;
+    const SCREEN_SM_MIN = 480;
 
     return (scope, element, attrs) => {
 
-      const property = attrs.resize ? (scope[attrs.resize] = {}) : scope;
+      let property = attrs.resize ? (scope[attrs.resize] = {}) : scope;
+
+      if (attrs.resizeFn) {
+        scope.resizeFn = scope.$eval(attrs.resizeFn);
+      }
+
+      let minHeight = attrs.resizeMinHeight || 0;
+
+      scope.resizeProperty = attrs.resizeProperty || 'max-height';
+      let offsetTopMinus = attrs.resizeOffsetTop ? parseInt(attrs.resizeOffsetTop) : 0;
+      let offsetTopMinusXs = attrs.resizeOffsetTopXs ? parseInt(attrs.resizeOffsetTopXs) : offsetTopMinus;
+
+      element.css('overflow-y', 'scroll');
+
+      function resizeOffsetTop(newValue) {
+        let offset = newValue.windowWidth > SCREEN_SM_MIN ? offsetTopMinus : offsetTopMinusXs;
+        if (!newValue || $rootScope.hasInputInFocus || newValue.disableResize) {
+          return;
+        }
+        let height = newValue.windowHeight - newValue.offsetTop - offset;
+        if (height < minHeight) {
+          height = minHeight;
+        }
+        element.css(scope.resizeProperty, height + 'px');
+      }
 
       function getWindowDimensions() {
-        const offset = $uibPosition.offset(element);
+        let offset = $uibPosition.offset(element);
+        //let bodyRect = $window.document.body.getBoundingClientRect();
         return {
-          windowHeight: $window.innerHeight,
+          windowHeight: $window.innerHeight,// - bodyRect.top,
           windowWidth: $window.innerWidth,
-          offsetTop: offset ? offset.top : 0
+          offsetTop: offset ? offset.top : 0,
+          disableResize: !!$rootScope.hasInputInFocus
         };
       }
 
       function setValues(newValue) {
+        if (!newValue || newValue.disableResize || $rootScope.hasInputInFocus) {
+          return;
+        }
         _.assign(property, newValue);
+        _.assign(property, {
+          xsWidth: _.get(newValue, 'windowWidth') < SCREEN_XS_MAX
+        });
+        if (scope.resizeFn) scope.resizeFn(property, element);
       }
 
-      let un = scope.$watch(getWindowDimensions, setValues, true);
+      let un = scope.$watch(
+        getWindowDimensions,
+        angular.isDefined(attrs.resizeOffsetTop) ? resizeOffsetTop : setValues,
+        true
+      );
 
-      function apply() {
+      let apply = _.throttle(() => {
         scope.$apply();
-      }
+      }, 100);
 
       angular.element($window)
         .bind('resize', apply);
 
-      scope.$on('$destroy', ()=> {
+      scope.$on('$destroy', () => {
         un();
         angular.element($window)
           .unbind('resize', apply);
@@ -41,7 +81,7 @@
 
   }
 
-  angular.module('ui.bootstrap.position')
+  angular.module('sistemium')
     .directive('resize', resize);
 
 })();
